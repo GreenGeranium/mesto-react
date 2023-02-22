c3cd0697e217df633e904a67dcbc35b07a4b72bc
Maxim Grassi <grassimacsim@yandex.ru>
2023-02-23 01:52:12 +0300

    fix: получить данные профиля с сервера

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,39 @@
+import { useEffect, useState } from "react";
+import api from "../utils/api.js";
+
 function Main(props) {
+  const [userName, setUserName] = useState("");
+  const [userDescription, setUserDescription] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
+
+  //установление данных профиля с сервера
+  useEffect(() => {
+    api.getUserInfo().then((data) => {
+      setUserName(data.name);
+      setUserAvatar(data.avatar);
+      setUserDescription(data.about);
+    });
+  }, []);
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-container" onClick={props.onEditAvatar}>
           <img
-            src="src/components/App#"
+            src={userAvatar}
             alt="Аватарка Жак-Ив Кусто"
             className="profile__avatar"
           />
         </div>
         <div className="profile__description">
-          <h1 className="profile__name"></h1>
+          <h1 className="profile__name">{userName}</h1>
           <button
             className="profile__edit-button"
             type="button"
             aria-label="Редактировать профиль"
             onClick={props.onEditProfile}
           ></button>
-          <p className="profile__subline"></p>
+          <p className="profile__subline">{userDescription}</p>
         </div>
         <button
           className="profile__add-button"
